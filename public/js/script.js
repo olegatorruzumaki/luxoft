5ec6f20c278ffdc2f4d72e9836fb9cbfe2178f00
Oleg <44000429@hsbk.nb>
2017-05-23 18:13:18 +0600

    save

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 var sliderElement = document.getElementById('sliderCarousel');
-var sliderStartPoint = 5;
+var sliderStartPoint = 0;
 var sliderInterval;
 
 function sliderFunc() {
     sliderInterval = setInterval(function () {
         sliderStartPoint--;
-        sliderElement.style.marginLeft = sliderStartPoint + "px";
-        if (sliderStartPoint < -665) {
-            sliderStartPoint = 5;
+        sliderElement.style.transform: translateX(sliderStartPoint + px);
+        if (sliderStartPoint < -660) {
+            sliderStartPoint = 0;
         }
     }, 100);
 }
