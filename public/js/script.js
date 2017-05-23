7c0c45b91191670c6a9dee82eed144712c2927e8
Oleg <44000429@hsbk.nb>
2017-05-23 18:00:46 +0600

    lastCommit допил

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
-var sliderElement = document.getElementsByClassName('header__slider-ul');
+var sliderElement = document.getElementById('sliderCarousel');
 var sliderStartPoint = 5;
 var sliderInterval;
 
 function sliderFunc() {
     sliderInterval = setInterval(function () {
         sliderStartPoint = sliderStartPoint - 1;
-        sliderElement[0].style.marginLeft = sliderStartPoint + "px";
+        sliderElement.style.marginLeft = sliderStartPoint + "px";
         if (sliderStartPoint < -665) {
             sliderStartPoint = 5;
         }
