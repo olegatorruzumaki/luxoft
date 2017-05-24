9731a0f8c0db0774e99111df3cc102bc3bb31df9
Oleg <44000429@hsbk.nb>
2017-05-24 16:01:58 +0600

    ok

--- public/js/newsAjax.js
+++ public/js/newsAjax.js
@@ -26,7 +26,7 @@
 //         var article = document.createElement('article');
 //         var span = document.createElement('span');
 //         var h2 = document.createElement('h2');
-//         var time = document.createElement('time');
+//         var time = document.createElement('time');юЧто
 //         var p = document.createElement('p');
 //         var a = document.createElement('a');
 //
@@ -53,16 +53,37 @@
 //
 // loadNews();
 
-$.getJSON( "news.json", function( news ) {
-    var i;
-    var newsCount = news.length;
 
-    for ( i = 0; i < newsCount; i++ ) {
-        $('#h2divNews').after('<article id="newsArticle-'+news[i].id+'" class="block-news__news">');
-        $('#newsArticle-'+news[i].id).append('<span id="newsSpan-'+news[i].id+'">');
-        $('#newsSpan-'+news[i].id).append('<h2 class="block-news__header">'+news[i].header+'</h2>');
-        $('#newsSpan-'+news[i].id).append('<time class="block-news__date">'+news[i].date+'</time>');
-        $('#newsArticle-'+news[i].id).append('<p>'+news[i].content+'</p>');
-        $('#newsArticle-'+news[i].id).append('<a class="block-news__more" href="#">more ></a>');
-    }
+
+
+
+
+// $.getJSON( "news.json", function( news ) {
+//     var i;
+//     var newsCount = news.length;
+//
+//     for ( i = 0; i < newsCount; i++ ) {
+//         $('#h2divNews').after('<article id="newsArticle-'+news[i].id+'" class="block-news__news">');
+//         $('#newsArticle-'+news[i].id).append('<span id="newsSpan-'+news[i].id+'">');
+//         $('#newsSpan-'+news[i].id).append('<h2 class="block-news__header">'+news[i].header+'</h2>');
+//         $('#newsSpan-'+news[i].id).append('<time class="block-news__date">'+news[i].date+'</time>');
+//         $('#newsArticle-'+news[i].id).append('<p>'+news[i].content+'</p>');
+//         $('#newsArticle-'+news[i].id).append('<a class="block-news__more" href="#">more ></a>');
+//     }
+// });
+
+
+
+$.getJSON('news.json', function(data) {
+
+    $.each(data, function(key, news) {
+
+        console.log(news.header);
+        $('#h2divNews').after('<article id="newsArticle-'+news.id+'" class="block-news__news">');
+        $('#newsArticle-'+news.id).append('<span id="newsSpan-'+news.id+'">');
+        $('#newsSpan-'+news.id).append('<h2 class="block-news__header">'+news.header+'</h2>');
+        $('#newsSpan-'+news.id).append('<time class="block-news__date">'+news.date+'</time>');
+        $('#newsArticle-'+news.id).append('<p>'+news.content+'</p>');
+        $('#newsArticle-'+news.id).append('<a class="block-news__more" href="#">more ></a>');
+    });
 });
